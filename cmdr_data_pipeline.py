8710cb661244bb9fd3ba063c00111247f4f0cf9f
sguthrie <sguthrieobx@gmail.com>
2021-01-16 11:06:34 -0500

    Successfully fetched all files from cmdr_data directory

--- cmdr_data_pipeline.py
+++ cmdr_data_pipeline.py
@@ -1,3 +1,7 @@
 print("Starting... CMDR DATA PIPELINE")
 from replace import *
 from logging import *
+from extract import *
+
+#Three letter tag for every Magic set
+mtg_set_dict = {}
--- extract.py
+++ extract.py
@@ -1,1 +1,13 @@
 # extract
+import os
+
+CMDR_RAW_FILES = []
+
+for file in os.listdir("./cmdr_data"):
+    if file.endswith(".txt"):
+        CMDR_RAW_FILES.append(file)
+        print(os.path.join("/cmdr_data", file))
+
+class Extract(object):
+    def __init__(self):
+        pass
