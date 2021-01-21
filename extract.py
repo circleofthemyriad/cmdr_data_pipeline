5eaf88e455fff6bc32190faa1d8b9a542a451bc5
circleofthemyriad <sguthrieobx@gmail.com>
2021-01-21 13:03:33 -0500

    Added new decklist

--- extract.py
+++ extract.py
@@ -12,7 +12,7 @@ CMDR_RAW_FILES = []
 for file in os.listdir("./cmdr_data"):
     if file.endswith(".txt"):
         CMDR_RAW_FILES.append(file)
-        print("Logging - Extraction Event: Found Deck List: " + file) # Replace with Logging
+        print("Logging - Extract Event: Found Deck List: " + file) # Replace with Logging
 
 CMDR_DECKS_NUM = len(CMDR_RAW_FILES)
 
@@ -22,6 +22,12 @@ class Extract(object):
         self.deckname = self.filename.rstrip('.txt')
         self.metaname = str(self.deckname+"__"+date.today().strftime("%d%m%y")+".csv")
 
+    def isNull(self, list):
+        if not list:
+            return False
+        else:
+            return True
+
     def createDeck(self):
         if os.path.exists('./cmdr_metadata/'+self.metaname):
             os.remove('./cmdr_metadata/'+self.metaname)
@@ -30,19 +36,27 @@ class Extract(object):
             for line in deck_file:
                 data = tuple(line.split())
 
+                if "Maybeboard" or "Sideboard" in line:
+                    card_indeck = False
+                else:
+                    card_indeck = True
+
                 with open("./cmdr_metadata/"+self.metaname,"a") as decklist:
-                    fieldnames = ['Quantity','Card Name','Set','Set Release']
+                    fieldnames = ['Quantity','Card Name','Set','Set Release','Foil','Main Deck']
                     writer     = csv.DictWriter(decklist,fieldnames=fieldnames)
 
                     card_quantity = data[0]
                     card_name     = re.search(r'\s(.*)\(',line).group(1)
                     card_set      = [i for i in data if i.startswith('(')]
                     card_release  = MTG_SET_DICT[card_set[0].upper()][1]
+                    card_foil     = [i for i in data if i.startswith('*')]
 
                     writer.writerow({'Quantity'   : card_quantity,
-                                     'Card Name'  : card_name.strip(' "\'\t\r\n'),
+                                     'Card Name'  : card_name.strip('\"\'\t\r\n '),
                                      'Set'        : MTG_SET_DICT[card_set[0].upper()][0],
-                                     'Set Release': card_release
+                                     'Set Release': card_release,
+                                     'Foil'       : self.isNull(card_foil),
+                                     'Main Deck'  : card_indeck
                                     })
-                                    
-        print("Logging - Extraction Event: Deck Complete : " + self.deckname)
+
+            print("Logging - Extract Event: Deck Complete : " + self.deckname)
